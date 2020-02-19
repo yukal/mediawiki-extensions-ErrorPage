1474f87e5f2073837843e427911036f2689fd705
Alexander Yukal <yukal@email.ua>
2020-02-19 20:25:28 +0200

    add files

---
+++ includes/Hooks.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * ErrorPage Hooks
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Alexander Yukal <yukal@email.ua>
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://www.mediawiki.org/wiki/Extension:ErrorPage Documentation
+ */
+
+namespace MediaWiki\Extension\ErrorPage;
+
+use MediaWiki\MediaWikiServices;
+use Article;
+use Title;
+use User;
+use SkinTemplate;
+use RequestContext;
+
+class Hooks {
+
+	public static function onSkinTemplateNavigation( SkinTemplate $skinTemplate, Array &$links ) {
+		if (!$skinTemplate->getContext()->getUser()->isLoggedIn() && 
+			!$skinTemplate->getTitle()->getArticleID()) {
+				$links = [];
+		}
+	}
+
+	public static function onBeforeDisplayNoArticleText(Article $article) {
+		$mwsInstance = MediaWikiServices::getInstance();
+		$wgSend404Code = $mwsInstance->getMainConfig()->get('Send404Code');
+
+		$title = $article->getTitle();
+		$validUserPage = $title->isSubpage();
+		$loggedIn = $article->getContext()->getUser()->isLoggedIn();
+		$sessionExists = $article->getContext()->getRequest()->getSession()->isPersistent();
+
+		if ( !$article->getPage()->hasViewableContent() && $wgSend404Code && !$validUserPage ) {
+			$out = $article->getContext()->getOutput();
+
+			$defaultStatusCode = 404;
+			$statusCode = $title->isDeleted() ?410 :404;
+			$out->setStatusCode( $defaultStatusCode );
+
+			if (!$loggedIn && !$sessionExists) {
+				$msgNotFound = wfMessage( 'errorpage-title', $defaultStatusCode );
+				$msgSubtitle = wfMessage( 'errorpage-status-'.$statusCode );
+	
+				$out->clearHTML();
+				$out->setPageTitle( $msgNotFound );
+				// $out->setHTMLTitle( $msgNotFound );
+				$out->addWikiMsg( 'errorpage-body', $msgSubtitle, $title->getBaseText());
+				// $out->addWikiMsg( 'noarticletext-nopermission' );
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static function onGetUserPermissionsErrors( Title $title, User $user, $action, &$result ) {
+		if (!$user->isLoggedIn() && $action=='edit') {
+			RequestContext::getMain()->getOutput()->setStatusCode( 401 );
+		}
+	}
+
+}
---
+++ includes/alias.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Aliases for ErrorPage
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$errorPageAliases = array();
+
+/** English (English) */
+$errorPageAliases['en'] = [
+	'Error404' => [ 'Error404', '404' ],
+];
+
+/** Russian (русский) */
+$errorPageAliases['ru'] = [
+	'Error404' => [ 'Ошибка_404' ],
+];
+
+/** Ukrainian (українська) */
+$errorPageAliases['uk'] = [
+	'Error404' => [ 'Помилка_404' ],
+];
